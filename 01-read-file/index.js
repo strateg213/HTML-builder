fb6a40d8bb9c7eb1157fdb8b4aedbf9690f1d3c1
strateg213 <strateg213@gmail.com>
2024-01-18 17:53:16 +0300

    feat: update files

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,11 @@
-const Stream = require('stream');
+//const Stream = require('stream');
 const fs = require('fs');
-const readableStream = new Stream.Readable();
-
-fs.readFile('./01-read-file/text.txt', {encoding: 'utf-8'}, (err, data) => {
-  if(err) {
+//const readableStream = new Stream.Readable();
+fs.readFile('./01-read-file/text.txt', { encoding: 'utf-8' }, (err, data) => {
+  if (err) {
     console.log(err);
     return;
   } else {
     console.log(data);
-    }
-})
+  }
+});
