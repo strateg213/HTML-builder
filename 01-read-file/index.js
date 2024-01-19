5c5a01c2308e6757c68d412110a18043f1d6ced4
strateg213 <strateg213@gmail.com>
2024-01-19 13:31:54 +0300

    refactor: fix code formatting

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
-const stream = fs.createReadStream(path.join(__dirname, "text.txt"), 'utf-8');
-stream.on("data", (data) => stdout.write(data));
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stream.on('data', (data) => stdout.write(data));
